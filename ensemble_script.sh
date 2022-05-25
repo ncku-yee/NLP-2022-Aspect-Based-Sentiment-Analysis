bfcfd8f5d6f77c0de467583f85c133b9500d99c7
ncku-yee <asd887722456@gmail.com>
2022-05-25 20:55:05 +0800

    bash script

---
+++ ensemble_script.sh
@@ -0,0 +1,4 @@
+python ensemble.py \
+    --task 1 \
+    --batch_size 8 \
+    --pretrained_model IDEA-CCNL/Erlangshen-Roberta-110M-Sentiment \
---
+++ predict_script.sh
@@ -0,0 +1,9 @@
+python train.py \
+    --task 2 \
+    --batch_size 8 \
+    --pretrained_model IDEA-CCNL/Erlangshen-Roberta-110M-Sentiment \
+    --model_path best_model_task2_roberta_110m_sentiment_083436.pt \
+    --output_path 123.csv \
+    --validation \
+    --verbose \
+    --is_train \
